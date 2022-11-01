a1e4c7a637e92bbbc2234f3afaa212b2a68881d1
Lee-na-yun <happyin3333@nate.com>
2022-11-01 16:44:14 +0900

    JSX 기본문법 공부 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+let a = 10; //변수
+const b = 20; //상수
+
 function App() {
-  return <div>안녕</div>;
+  let c;
+  console.log(1, c);
+
+  const mystyle = {
+    color: 'red',
+  };
+
+  return (
+    <div>
+      <div style={mystyle}>안녕{a === 10 ? '10입니다.' : '10이 아닙니다.'}</div>
+      <h1 className="box-style">해당태그{b === 20 && '20입니다.'}</h1>
+    </div>
+  );
 }
 
 export default App;
