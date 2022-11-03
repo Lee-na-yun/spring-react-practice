f9cb68bc2b75484a4f953b21fdabeeecfb66106d
Lee-na-yun <happyin3333@nate.com>
2022-11-03 15:35:45 +0900

    import 하는 방법

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
-import { useState } from 'react';
 import './App.css';
+import Sub from './Sub';
+import {num} from './Sub';
 
+// map, filter, concat, spread, slice
 function App() {
-  // 다운로드 받음 -> user데이터를 상태로 관리하고 싶음
-  console.log('App 실행됨');
-
-  let sample = [
-    { id: 1, name: '홍길동' },
-    { id: 2, name: '임꺽정' },
-    { id: 3, name: '장보고' },
-    { id: 4, name: '이순신' },
-  ];
-
-  const [users, setUsers] = useState(sample);
-
-  const download = () => {
-    // 기존 데이터 세팅
-    const a = sample.concat({ id: 5, name: '조자룡' });
-    setUsers(a);
-  };
-
-  // 랜더링 시점 = 상태값 변경
+  
+  
+  
   return (
-    <div>
-      <button onClick={download}>다운로드</button>
-      {users.map((users) => (
-        <h1>
-          {users.id},{users.name}
-        </h1>
-      ))}
-    </div>
+    <div>Hello World {num}</div>
   );
 }
 
