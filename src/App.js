bdb915d484fa06f3aea49d73781d3d689f67175c
Lee-na-yun <happyin3333@nate.com>
2022-11-03 16:32:33 +0900

    useEffect의 dependencyList까지 공부 완료

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
 
   const [data, setData] = useState(0);
+  const [search, setSearch] = useState(0);
 
   const download = ()=>{
     // 다운로드 받고 (통신을 통해서)
@@ -17,10 +18,11 @@ function App() {
   useEffect(()=>{
     console.log("useEffect 실행됨");
     download();
-  },[ ]);
+  },[search]);
 
   return (
     <div>
+      <button onClick={()=>{setSearch(2)}}>검색하기</button>
       <h1>데이터 : {data}</h1>
       <button onClick={()=>{setData(data+1)}}>더하기</button>
     </div>
