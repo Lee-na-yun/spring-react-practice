4f2b45321e1048eadef1b4a81ab2dad3632b233c
Lee-na-yun <happyin3333@nate.com>
2022-11-03 16:12:00 +0900

    useEffect 개념 이해하기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import Sub from './Sub';
-import {num} from './Sub';
 
 // map, filter, concat, spread, slice
 function App() {
-  
-  
-  
+
+  const [data, setData] = useState(0);
+
+  const download = ()=>{
+    // 다운로드 받고 (통신을 통해서)
+    let downloadData = 5; //가정
+    // 받은 데이터 넣기
+    setData(downloadData);
+  }
+
+  // 실행시점 :  (1) App()함수가 최초 실행될 때
+  useEffect(()=>{
+    console.log("useEffect 실행됨");
+    download();
+  },[ ]);
+
   return (
-    <div>Hello World {num}</div>
+    <div>
+      <h1>데이터 : {data}</h1>
+      <button onClick={()=>{setData(data+1)}}>더하기</button>
+    </div>
   );
 }
 
