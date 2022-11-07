1188aa506860ae56822941e6d5c91907cb80c671
Lee-na-yun <happyin3333@nate.com>
2022-11-07 16:44:51 +0900

    내부에서 styled-components props하는 예제

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,12 @@ import styled from 'styled-components';
 const HeaderMenu = styled.div`
   border: 1px solid black;
   height: 300px;
+  background-color: ${(props) => props.backgroundColor};
 `;
 
 const Header = () => {
   return (
-    <HeaderMenu>
+    <HeaderMenu backgroundColor="blue">
       <ul>
         <li>메뉴1</li>
         <li>메뉴2</li>
