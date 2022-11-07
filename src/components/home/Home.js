b2107a3f838d2a6941d573f8dea88617caba69aa
Lee-na-yun <happyin3333@nate.com>
2022-11-07 16:48:23 +0900

    styled-components props패싱 예제 수정 완료-외부에서 값 넘기기

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const StyledDeleteButton = styled.button`
+  color: ${(props) => (props.user.username === 'ssar' ? 'blue' : 'red')};
+`;
+
 // Function 방식
 const Home = (props) => {
   // 구조분할 할당
   const { boards, setBoards, number, setNumber, user } = props;
 
-  const StyledDeleteButton = styled.button`
-    color: ${() => (user.username === 'ssar' ? 'blue' : 'red')};
-  `;
-
   return (
     <div>
       <h1>홈: {number}</h1>
       <button onClick={() => setNumber(number + 1)}>번호증가</button>
-      <StyledDeleteButton onClick={() => setBoards([])}>
+      <StyledDeleteButton user={user} onClick={() => setBoards([])}>
         전체 삭제
       </StyledDeleteButton>
       {boards.map((board) => (
